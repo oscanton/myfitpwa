36e4245e651d5cc381d8ff72c6471e94e5ee7623
oscanton <oscanton@gmail.com>
2026-01-12 20:07:31 +0100

    Add files via upload

--- js/core/pwa.js
+++ js/core/pwa.js
@@ -1,7 +1,12 @@
 // js/core/pwa.js
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js")
+    // Desde index.html -> "sw.js"
+    // Desde views/*.html -> "../sw.js"
+    // new URL() lo resuelve correctamente.
+    const swUrl = new URL("./sw.js", window.location.href).toString();
+
+    navigator.serviceWorker.register(swUrl)
       .catch(err => console.error("SW register failed:", err));
   });
 }
